ff0cb8fb5f4d426f1018b9fb84b8696ad7347c1a
Mohsin Ahmed Khan <136707643+MohsinKhan667@users.noreply.github.com>
2024-05-06 14:24:50 -0700

    Add files via upload

---
+++ Assigment-2/app.js
@@ -0,0 +1,117 @@
+// Chapter 4
+
+// Q 1
+
+var a , b , c
+
+// Q 2
+
+// var name 
+// var _name
+// var yourName
+// var name123
+// var $name
+
+
+// var 222name
+// var -name
+// var YourName
+// var my@var
+// var var
+
+// Q 3
+
+// a)
+var abc = "“Rules for naming JS variables”"  
+document.write ("<h1>"+abc+"</h1>")
+
+// b)
+var  contain = "Variable names can only contain number, $ and __ for example : $my __ 1stVariable"
+document.write ("<h3>"+contain+"</h3>")
+
+// c)
+var must = "Variables must begin with a letter, $ or __. For example : $name, _name or name"
+document.write ("<h3>"+must+"</h3>")
+
+// d)
+var name = "Variable names are Case sensitive"
+document.write("<h3>"+name+"</h3>")
+
+// e)
+var key = "Variable names should not be JS keywords"
+document.write ("<h3>"+key+"</h3>")
+
+
+// Chapter 5
+
+// Q 1
+
+var num1 = 5
+var num2 = 10
+ 
+var add =  num1 + num2
+
+document.write (" sum of "+ num1 + " and " + num2 + " is "+ add + "<br/>")
+
+// Q 2
+
+var sub =  num1 - num2
+
+document.write (" subtraction of "+ num1 + " and " + num2 + " is "+ sub + "<br/>")
+
+var multi = num1 * num2
+
+document.write (" multiplication of " + num1 + " and " + num2 + " is " + multi + "<br/>")
+
+var divide = num1 / num2
+
+document.write (" divide of " + num1 + " and " + num2 + " is " + divide + "<br/>")
+
+var reminder = num1 % num2
+
+document.write (" reminder of " + num1 + " and " + num2 + " is " + reminder + "<br/>")
+
+// Q 3
+
+var abc;
+
+document.write(" Value after variable declaration is " + abc + "<br/>")
+
+abc = 5
+
+document.write("Initial value " + abc + "<br/>")
+ 
+abc++
+
+document.write("value after increment is " + abc + "<br/>")
+
+var add = abc + 7
+
+document.write("value after addition is " + add + "<br/>")
+
+add--
+
+document.write("value after decrement is " + add + "<br/>")
+
+var modulus = add% 3
+
+document.write("The remainder is " + modulus + "<br/>")
+
+// Q 4
+
+var ticket = 600
+var buying = 5
+var cost = ticket*buying
+document.write ("Total cost to buy "+ buying + " ticket to a movie is " + cost + "PKR"  + "<br/>")
+
+// Q 6
+
+var Celsius_Temp = 30
+var fahrenheit = (Celsius_Temp * 9/5)+ 32
+document.write( Celsius_Temp + "into fahrenheit is " + fahrenheit+ "<br/>")
+
+ var fahrenheit_Temp = 50 
+ var Celsius = (fahrenheit_Temp - 32 ) * 5/9
+document.write( fahrenheit_Temp + " into celsius is " + Celsius)
+
+ 
