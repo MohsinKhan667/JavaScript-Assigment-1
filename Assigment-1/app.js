9775ad980bdd4a659fa561b1fab593059b7de0dc
Mohsin Ahmed Khan <136707643+MohsinKhan667@users.noreply.github.com>
2024-05-06 14:34:16 -0700

    Add files via upload

---
+++ Assigment-1/app.js
@@ -0,0 +1,57 @@
+
+// Chapter 1
+
+// Q1
+alert('Error! Please enter a valid password')
+
+// Q2
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// Q3
+alert("Welcome to JS Land...")
+
+// Q4
+alert("Happy Coding! \nprevent this page from creating additional dialogs")
+
+// Q5
+alert("Hello... I can run JS through my web browser's console")
+
+
+// Chapter 2
+
+// Q1
+var username = ("Jhone Doe")
+alert(username)
+
+// Q2
+var age = ("15 years old")
+alert(age)
+
+// Q3
+var certificate = ("Certified Mobile Application Development")
+alert(certificate)
+
+// Q4
+var pizza = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+alert(pizza)
+
+// Q5
+var mail = ("My email address is mohsinahemdkhan667@gmail.com")
+alert(mail)
+
+// Q6
+var text =("I am trying to learn from the Book A Smarter way to learn JavaScript")
+alert (text)
+
+
+// Chapter 3
+
+// Q1
+var age =("I am 15 years old")
+
+alert (age)
+
+// Q2
+var times =("You have visited this site 14 times")
+
+alert (times)
