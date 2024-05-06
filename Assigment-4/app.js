11061c4827e846e0486057a7816af27d270a2eda
Mohsin Ahmed Khan <136707643+MohsinKhan667@users.noreply.github.com>
2024-05-06 13:14:37 -0700

    Add files via upload

---
+++ Assigment-4/app.js
@@ -0,0 +1,175 @@
+                                      // chapter 15 and 16
+
+// Q:5
+
+// var studentName = []
+// studentName[0] = "Mohsin"
+// console.log(studentName)
+
+// Q:4
+
+// var studentNames = new Arr4y();
+// console.log(studentNames);
+
+
+// var obj = {
+//     "status_code": 200,
+//     "message": "Data Get Successfully",
+//     "data": {
+//     "name": "Mohsin",
+//     "age": 18,
+//     "Email": "mohsinahmed9111@gmail.com",
+//     "adress": {
+//             "lat" : 121254.00,
+//             "long": 12888.220,
+//             "country":"Pakistan",
+//             "city":"Karachi",
+//             "postal_code":75800
+//         }
+//     }
+// }
+// console.log(obj["data"].adress.country)
+
+// for(var i in obj) {
+//     console.log(obj[i])
+// }
+// console.log(obj["age"])
+
+
+// Q 4
+// var arr = []
+
+// var obj = 4
+//     "studentName": "Mohsin"
+// }
+
+// arr[0] = obj;
+// console.log(arr[0].studentName)
+
+
+//  Q 3
+// var strArr = []
+
+// strArr[0] = "Mohsin",
+// strArr[1] = "Hasnain"
+// strArr[2] = "Moeez"
+
+// console.log(strArr)
+
+
+// Q 4
+// var num = []
+
+// num[0] = 10
+// num[1] = 20
+// num[2] = 30
+
+// console.log(num)
+
+// Q 5
+// var booLean = []
+
+// booLean[0] = true
+// booLean[1] = false
+
+// console.log(booLean)
+
+// Q 6
+// var mixedArr = []
+
+// mixedArr[0] = "Mohsin";
+// mixedArr[1] = 18;
+// mixedArr[2] = 2.8;
+// mixedArr[3] = true;
+// mixedArr[4] = false;
+
+// console.log(mixedArr)
+
+// Q 7
+// var QualificationsArr = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","PhD"];
+
+// document.write("Qualifications:" + "<br/>")
+
+// for(var i = 0; i < QualificationsArr.length; i++){
+//     document.write((i + 1 +")")  + " " + QualificationsArr[i] + "<br/>")
+// }
+
+
+// Q 8
+// var studentName = ["Micheal", "John", "Tony"]
+// var score = [320, 230, 480]
+
+// console.log("score of " + studentName[0] + " is " + score[0] + " percentage: " + score[0]/500*100 + "%")
+// console.log("score of " + studentName[1] + " is " + score[1] + " percentage: " + score[1]/500*100 + "%")
+// console.log("score of " + studentName[2] + " is " + score[2] + " percentage: " + score[2]/500*100 + "%")
+
+// for(var i = 0; i < studentName.length; i++){
+//     console.log("score of " + studentName[i] + " is " + score[i] + " percentage " + score[i]/500*100 + "%")
+// }
+
+// Q 10
+// var studentScore = [ 320, 230, 480, 120]
+
+// console.log("scroes of students: " + studentScore )
+// console.log("ordered scroes of students: " + studentScore.sort())
+
+
+// Q 11
+// var cityName = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+// document.write("city list: <br/>" + cityName  + "<br>");
+// document.write("selected cities list: <br/>" + cityName.slice(2,4));
+
+
+// Q 12
+// var arr = ["This", "is", "my", "cat"]
+
+// document.write("Array: <br/>" + arr + "<br/>")           
+// document.write("String: <br/>" + arr.join(" ")) 
+ 
+
+// Q 13
+// var newArr = ["keyboard", "mouse", "printer", "monitor"]
+// document.write("Devices: <br/>"+ newArr)
+
+// var arr = []
+// arr.push("keyborad")
+// arr.push("mouse")
+// arr.push("printer")
+// arr.push("monitor")
+
+// document.write("<br> Out: <br>" + arr.shift())
+// document.write("<br> Out: <br>" + arr.shift())
+// document.write("<br> Out: <br>" + arr.shift())
+// document.write("<br> Out: <br>" + arr.shift())
+
+
+// // Q 14 
+// var lifo = ["keyboard", "mouse", "printer", "monitor"]
+// document.write("Device: <br>" + lifo)
+
+// var arr = []
+// arr.push("keyboard")
+// arr.push("mouse")
+// arr.push("printer")
+// arr.push("monitor")
+
+// document.write("<br> Out: <br>" + arr.pop())
+// document.write("<br> Out: <br>" + arr.pop())
+// document.write("<br> Out: <br>" + arr.pop())
+// document.write("<br> Out: <br>" + arr.pop())
+
+
+// Q 15
+// var phoneManufacturers = ["Äpple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"]
+
+// document.write('<select name="Manufacturers" id="Manufacturers">');
+
+// document.write('<option value="' + phoneManufacturers[0] + '">' + phoneManufacturers[0] + '</option>')
+// document.write('<option value="' + phoneManufacturers[1] + '">' + phoneManufacturers[1] + '</option>');
+// document.write('<option value="' + phoneManufacturers[2] + '">' + phoneManufacturers[2] + '</option>')
+// document.write('<option value="' + phoneManufacturers[3] + '">' + phoneManufacturers[3] + '</option>');
+// document.write('<option value="' + phoneManufacturers[4] + '">' + phoneManufacturers[4] + '</option>')
+// document.write('<option value="' + phoneManufacturers[5] + '">' + phoneManufacturers[5] + '</option>');
+
+ 
